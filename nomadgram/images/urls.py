f6da96c726e6aeefc435dddc47ca3858aef9c732
Sangwook Cho <korean.crossfitter@gmail.com>
2018-02-07 01:43:05 +0900

    Upload image

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
 
     url(
         regex=r'^$',
-        view=views.Feed.as_view(),
-        name='feed'
+        view=views.Images.as_view(),
+        name='images'
     ),
     url(
         regex=r'^(?P<image_id>\d+)/$',
