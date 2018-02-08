fc3645b3da5f3e33961b969ea0be5afbcbff2b88
Sangwook Cho <korean.crossfitter@gmail.com>
2018-02-08 17:04:12 +0900

    Redux dev tools

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,11 +1,44 @@
-import { createStore, combineReducers } from 'redux'
-import users from './modules/users'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { routerReducer, routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import users from 'redux/modules/users'
+import Reactotron from 'ReactotronConfig'
+
+const env = process.env.NODE_ENV
+
+const history = createHistory()
+
+const middlewares = [thunk, routerMiddleware(history)]
+
+if (env === 'development') {
+  const { logger } = require('redux-logger')
+  middlewares.push(logger)
+}
 
 const reducer = combineReducers({
-  users: users
+  users: users,
+  routing: routerReducer
 })
 
-let store = initialState => createStore(reducer)
+let store
+
+if (env === 'development') {
+  store = initialState =>
+    Reactotron.createStore(
+      reducer,
+      composeWithDevTools(applyMiddleware(...middlewares))
+    )
+} else {
+  store = initialState => createStore(
+    reducer, applyMiddleware(...middlewares)
+  )
+}
+
+
+
+export { history }
 
 export default store()
 
